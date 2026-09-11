656dc21c76adc2392280fba491a981dc5d423659
Andrzej-Napora <andrzej6453@gmail.com>
2026-09-11 10:25:22 +0000

    added gold tests

---
+++ databricks/nyc_tlc_pipeline/tests/test_gold.py
@@ -0,0 +1,83 @@
+dfy = spark.table('workspace.silver.yellow_trip_records')
+dfg = spark.table('workspace.silver.green_trip_records')
+dffhv = spark.table('workspace.silver.fhv_trip_records')
+dfhv = spark.table('workspace.silver.fhvhv_trip_records')
+dfgold = spark.table('workspace.gold.unioned_tables')
+
+error = False
+
+# test 1 - record count consistency
+silver = [dfy,dfg,dffhv,dfhv]
+
+silver_count = sum(table.count() for table in silver)
+gold_count = dfgold.count()
+
+if silver_count == gold_count:
+    print("test finished successfully")
+else:
+    print("records count do not match")
+    error = True
+
+# test 2 - data_type content
+
+silver_data_type = set()
+for df in silver:
+    silver_data_type.update(row['data_type'] for row in df.select('data_type').distinct().collect())
+
+gold_data_type = {row['data_type'] for row in dfgold.select('data_type').distinct().collect()}
+
+if silver_data_type == gold_data_type:
+    print("test finished successfully")
+else:
+    print("data_type content do not match")
+    error = True
+
+# test 3 - schema consistency
+
+for df in silver:
+    if df.schema != dfgold.schema:
+        print("schema content do not match")
+        error = True
+    else:
+        print("test finished successfully")
+
+# test 4 - datetime consistency
+
+row_count = dfgold.filter("pickup_datetime>dropoff_datetime").count()
+
+if row_count==0:
+    print("test finished successfully")
+else:
+    print("pickup_datetime is greater than dropoff_datetime")
+    error = True
+
+# test 5 - factual values not null
+
+row_count_fv = dfgold.filter(
+        """pickup_datetime is null
+        or dropoff_datetime is null
+        or trip_distance is null
+        or pu_location_id is null
+        or do_location_id is null"""
+    ).count()
+
+if row_count_fv==0:
+    print("test finished successfully")
+else:
+    print("factual value is null")
+    error = True
+
+# test 6 - valid location code
+
+location_code_count = dfgold.filter(
+    "pu_location_id not between 1 and 265 or do_location_id not between 1 and 265"
+    ).count()
+
+if location_code_count==0:
+    print("test finished successfully")
+else:
+    print("invalid location code")
+    error = True
+
+
+assert error == False, 'gold test failed'
